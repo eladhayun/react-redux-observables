281e3621a66673943e8b1befdcba317a37d3b01b
Elad Hayun <elad.hayun@gmail.com>
2018-07-14 21:55:37 +0300

    Change local URL to Mocky

--- src/epics/versionEpic.js
+++ src/epics/versionEpic.js
@@ -4,7 +4,7 @@ import { VERSION_REQUESTED, versionReceived } from '../actions'
 export const requestVersionEpic = action$ => {
   return action$.ofType(VERSION_REQUESTED).switchMap(() => {
     return Observable.ajax
-      .getJSON('http://localhost:5000/version')
+      .getJSON('http://www.mocky.io/v2/5b4a46fd2f000077001e0e3a')
       .delay(2000)
       .map(versionReceived)
   })
