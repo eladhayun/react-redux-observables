8f00f8314a6c996a6cf64f1a1cddd810a419926b
Elad Hayun <elad.hayun@gmail.com>
2019-03-22 10:47:36 +0200

    Add a fallback target to API proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
   app.use(
     proxy('/api', {
-      target: process.env.API_SERVER,
+      target: process.env.API_SERVER || 'http://www.example.org',
       pathRewrite: {
         '^/api': ''
       },
